46b325f8c943668d9f2d6c21f077746ed029518e
ouhab <mohamed-moh98@live.com>
2023-09-19 00:35:30 +0100

    fix isAdmin in credit card router

--- src/routes/credit-card-routes.js
+++ src/routes/credit-card-routes.js
@@ -3,7 +3,7 @@ import express from "express";
 import creditCardController from "../controllers/credit-card-controller.js";
 import CreditCardValidationRules from "../validations/credit-card-validation.js";
 import { validate } from "../middlewares/validation-middleware.js";
-import { isAdmin } from "../middlewares/validation-middleware.js";
+import { isAdmin } from "../middlewares/authorization-middleware.js";
 const router = express.Router();
 
 // Define the route for retrieving filtered credit card data
